c710a3e1c60b831232a38cb8564ca7b91bf132af
kamaleshwaran-sudo <kamaleshwaran.l@aagnia.com>
2024-11-09 16:14:12 +0530

    Delete task/todo directory

--- task/todo/dist/todo/todo.interface.d.ts
+++
@@ -1,5 +0,0 @@
-export interface Todo {
-    id?: number;
-    label: string;
-    complete: boolean;
-}
--- task/todo/dist/todo/todo.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Todo } from './todo.interface';
-export declare class TodoService {
-    private todos;
-    findAll(): Todo[];
-    create(todo: Todo): Todo;
-    update(id: number, todo: Todo): Todo;
-    remove(id: number): Todo[];
-}
